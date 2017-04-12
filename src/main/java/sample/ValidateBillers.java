69568505842a5bcff8e92338030b5cb4ad75ce4f
Ugochukwu Okeke <ugochukwu.okeke@Interswitchng.com>
2017-04-12 15:11:04 +0100

    IA-6276

---
+++ src/main/java/sample/ValidateBillers.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class ValidateBillers extends BaseSample {
+
+    public static void main(String[] args){
+        
+        try{
+            //billPayment.validateBiller();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
